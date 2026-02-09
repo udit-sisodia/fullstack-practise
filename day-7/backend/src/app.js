790f7a1fc75226ce4e3d675b2428f233f22bcb87
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-09 12:49:31 +0530

    Track backend files correctly

---
+++ day-7/backend/src/app.js
@@ -0,0 +1,52 @@
+const express=require("express")
+const noteModel=require("../models/note.model")
+const cors=require("cors")
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+app.post("/api/notes",async (req,res)=>{
+    const {title,description}=req.body;
+    const note=await noteModel.create({
+        title,
+        description
+    })
+
+    res.status(201).json({
+        message:"note created successfully",
+        note
+    })
+})
+
+app.get("/api/notes",async (req,res)=>{
+    const notes=await noteModel.find()
+    res.status(200).json({
+        message:"notes retrieved succesfully",
+        notes
+    })
+})
+
+app.delete("/api/notes/:id",async (req,res)=>{
+    const id=req.params.id;
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"note deleted succesfully"
+    })
+})
+
+app.patch("/api/notes/:id",async (req,res)=>{
+    const id=req.params.id;
+    const {description}=req.body;
+
+    await noteModel.findByIdAndUpdate(id,{description})
+
+    res.status(200).json({
+        description
+    })
+})
+
+
+module.exports=app;
