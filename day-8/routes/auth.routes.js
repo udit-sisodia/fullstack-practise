21e3ccf8bda69f795dda41869baf8f4a3d8c812d
udit-sisodia <uditsisodia1234@gmail.com>
2026-02-12 14:10:26 +0530

    env restored

--- day-8/routes/auth.routes.js
+++ day-8/routes/auth.routes.js
@@ -18,21 +18,50 @@ authRouter.post("/register", async (req, res) => {
     const user = await noteModel.create({
         name, email, password
     })
-    
+
     const token = jwt.sign({
         id: user._id,
-        email:user.email
+        email: user.email
 
     },
-    process.env.JWT_SECRET
+        process.env.JWT_SECRET
     )
 
-    res.cookie("jwt_token",token)
+    res.cookie("jwt_token", token)
 
     res.status(201).json({
         message: "registered successfully",
         user,
         token
+    })
+})
+
+authRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await noteModel.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({
+            message: "user not exist with this email address"
+        })
+    }
+
+    const isPasswordMatch = user.password === password
+
+    if (!isPasswordMatch) {
+        return res.status(401).json({
+            message: "invalid password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+    res.status(201).json({
+        message: "logged in sucessfully",
+        user
     })
 })
 
